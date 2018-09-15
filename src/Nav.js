70ea923ad70dcd7a9478adf9170ffebfd1b7fb36
Jeff Appareti <jappareti@gmail.com>
2018-09-15 11:35:57 -0700

    Updated styles from dark to white theme

--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Nav.css";
+
 function Nav() {
   return (
     <header className="App-header">
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/newest">Newest</Link>
-          </li>
-        </ul>
+        <Link to="/">Home</Link>
+        <Link to="/newest">Newest</Link>
       </nav>
     </header>
   );
